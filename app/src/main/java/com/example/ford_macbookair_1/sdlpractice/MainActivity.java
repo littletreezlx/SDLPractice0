01ff9a914988684cdb25e57daa102644b0e67b0d
vincent <602788458@qq.com>
2018-04-25 16:36:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ford_macbookair_1/sdlpractice/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ford_macbookair_1.sdlpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ford_macbookair_1.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
